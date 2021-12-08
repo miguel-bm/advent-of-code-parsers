d941d1e2f669a00e922ccce4a84822dc8b15ad61
miguel-bm <miguel.blanco.marcos@gmail.com>
2021-12-08 19:40:53 +0100

    chore(docs): Add docstrings

--- aocp/parsers.py
+++ aocp/parsers.py
@@ -50,7 +50,13 @@ class IntParser(BaseTransformParser):
 
 
 class BoolParser(BaseTransformParser):
-    def __init__(self, true: str = None, false: str = None):
+    def __init__(self, true: str = "1", false: str = "0"):
+        """Initializes a parser which parses a string into a boolean.
+
+        Args:
+            true (str, optional): The string to interpret as True. Defaults to "1".
+            false (str, optional): The string to interpret as False. Defaults to "0".
+        """
         self.true = true or "1"
         self.false = false or "0"
 
@@ -61,11 +67,16 @@ class BoolParser(BaseTransformParser):
         elif string == self.false:
             return False
         else:
-            raise ValueError(f"{string} is not a boolean")
+            raise ValueError(f"{string} cannot be interpreted as boolean")
 
 
 class CustomTransform(BaseTransformParser):
     def __init__(self, function: Callable[[str], X]):
+        """Initializes a parser which parses a string using a custom function.
+
+        Args:
+            function (Callable[[str], X]): The function to use to parse the string.
+        """
         self.function = function
 
     def parse(self, string: str) -> X:
@@ -74,6 +85,12 @@ class CustomTransform(BaseTransformParser):
 
 class SortTransform(BaseTransformParser):
     def __init__(self, reverse: bool = False, key: Callable[[X], Any] = None):
+        """Initializes a parser which sorts a string.
+
+        Args:
+            reverse (bool, optional): Whether to reverse the sort. Defaults to False.
+            key (Callable[[X], Any], optional): A function to use to sort the string. Defaults to None.
+        """
         self.reverse = reverse
         self.key = key
 
@@ -83,6 +100,11 @@ class SortTransform(BaseTransformParser):
 
 class MapTransform(BaseTransformParser):
     def __init__(self, mapping: Mapping[str, X]):
+        """Initializes a parser which maps a string to another object.
+
+        Args:
+            mapping (Mapping[str, X]): A mapping of strings to objects.
+        """
         self.mapping = mapping
 
     def parse(self, string: str) -> X:
@@ -91,6 +113,11 @@ class MapTransform(BaseTransformParser):
 
 class ReplaceTransform(BaseTransformParser):
     def __init__(self, mapping: Mapping[str, str]):
+        """Initializes a parser which replaces sets of characters in a string with other sets of characters.
+
+        Args:
+            mapping (Mapping[str, str]): A mapping of strings to replace to strings to replace with.
+        """
         self.mapping = mapping
 
     def parse(self, string: str) -> str:
@@ -101,6 +128,11 @@ class ReplaceTransform(BaseTransformParser):
 
 class ChainParser(BaseTransformParser):
     def __init__(self, parsers: list[BaseAoCParser]):
+        """Initializes a parser applies multiple parsers sequentially.
+
+        Args:
+            parsers (list[BaseAoCParser]): A list of parsers to apply sequentially.
+        """
         self.parsers = parsers
 
     @classmethod
@@ -120,13 +152,25 @@ class BaseIterableParser(BaseAoCParser):
         subparser: Union[BaseAoCParser, Callable] = None,
         splitter: Union[str, Sequence[str]] = None,
     ):
+        """Initializes a parser which parses a string into a sequence.
+
+        Args:
+            subparser (Union[BaseAoCParser, Callable], optional): The parser to use to parse each element of the sequence. Defaults to None.
+            splitter (Union[str, Sequence[str]], optional): The string or set of strings to split the string by. If not specified, the function will try to guess the splitter. Defaults to None.
+        """
         self.splitter = splitter
         self.subparser = subparser or str
 
     @classmethod
     def _decide_splitter(cls, string: str, priority: list[str] = None) -> str:
-        """
-        Returns the splitter that is most likely to be used in the string.
+        """Decides which splitter to use based on the priority list.
+
+        Args:
+            string (str): The string to split.
+            priority (list[str], optional): The list of splitters to use. Defaults to None.
+
+        Returns:
+            str: The splitter string to use.
         """
         priority = priority or cls.SPLITTER_PRIORITY
         for s in priority:
@@ -138,6 +182,16 @@ class BaseIterableParser(BaseAoCParser):
     def _split(
         cls, string: str, splitter: Union[str, Sequence[str]], count: int = None
     ) -> Sequence[str]:
+        """Splits a string by a splitter.
+
+        Args:
+            string (str): The string to split.
+            splitter (Union[str, Sequence[str]]): The string or set of strings to split the string by.
+            count (int, optional): The number of splits to return. If not specified, all splits are returned. If not specified, the string is returned as-is. Defaults to None.
+
+        Returns:
+            Sequence[str]: The split strings.
+        """
         if not splitter:
             return (c for c in string)
         if isinstance(splitter, str):
@@ -161,6 +215,17 @@ class BaseIterableParser(BaseAoCParser):
         ],
         count: int = None,
     ) -> Sequence:
+        """Parses a string into an iterable.
+
+        Args:
+            string (str): The string to parse.
+            splitter (Union[str, Sequence[str]]): The string or set of strings to split the string by.
+            subparser (Union[ BaseAoCParser, Callable, Sequence[Union[BaseAoCParser, Callable]] ]): The parser to use to parse each element of the sequence, or the parsers to use to parse each corresponding element of the sequence.
+            count (int, optional): The number of splits to return. If not specified, all splits are returned. Defaults to None.
+
+        Returns:
+            Sequence: The parsed iterable.
+        """
         splitter = splitter or cls._decide_splitter(string)
         if isinstance(subparser, Iterable):
             count = len(subparser) - 1 if count is None else count
@@ -210,6 +275,14 @@ class TupleParser(BaseIterableParser):
         format: str = None,
         dataclass=None,
     ):
+        """Initializes a parser which parses a string into a tuple.
+
+        Args:
+            subparser (Union[ BaseAoCParser, Callable, Sequence[Union[BaseAoCParser, Callable]] ], optional): The parser to use to parse each element of the sequence, or the parsers to use to parse each corresponding element of the sequence. If not specified, the string is returned as-is. Defaults to None.
+            splitter (Union[str, Sequence[str]], optional): The string or set of strings to split the string by. If not specified, the function will try to guess the splitter. Defaults to None. Cannot be used if format is specified.
+            format (str, optional): The format string to use to parse the tuple using the parse library. Defaults to None. Cannot be used with splitter.
+            dataclass ([type], optional): An optional dataclass to use to interpret the tuple. Defaults to None.
+        """
         assert (format is None) or (
             splitter is None
         ), "Cannot specify both splitter and parse format"
@@ -225,6 +298,16 @@ class TupleParser(BaseIterableParser):
             BaseAoCParser, Callable, Sequence[Union[BaseAoCParser, Callable]]
         ],
     ) -> tuple:
+        """Parses a string using a format string.
+
+        Args:
+            format (str): The format string to use.
+            string (str): The string to parse.
+            subparser (Union[ BaseAoCParser, Callable, Sequence[Union[BaseAoCParser, Callable]] ]): The parser to use to parse each element of the sequence, or the parsers to use to parse each corresponding element of the sequence. If not specified, the string is returned as-is.
+
+        Returns:
+            tuple: The parsed tuple.
+        """
         sequence = parse.parse(format, string)
         if isinstance(subparser, Iterable):
             assert len(subparser) == len(
@@ -253,6 +336,14 @@ class DictParser(BaseIterableParser):
         sequence_splitter: str = None,
         key_value_splitter: str = None,
     ):
+        """Initializes a parser which parses a string into a dictionary.
+
+        Args:
+            key_parser (Union[BaseAoCParser, Callable], optional): The parser to use to parse the keys. If not specified, the keys are returned as strings. Defaults to None.
+            value_parser (Union[BaseAoCParser, Callable], optional): The parser to use to parse the values. If not specified, the values are returned as strings. Defaults to None.
+            sequence_splitter (str, optional): The string to split the string by to get the key-value pairs. If not specified, the function will try to guess the splitter. Defaults to None.
+            key_value_splitter (str, optional): The string to split the key-value pair by. If not specified, the function will try to guess the splitter. Defaults to None.
+        """
         self.sequence_splitter = sequence_splitter
         self.key_value_splitter = key_value_splitter
         self.key_parser = key_parser or str
